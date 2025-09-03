b0fd455f5eda905c2cd2d0a584f29f348dd7ac12
Nappster2208 <piamaulanaibrahim@gmail.com>
2025-09-04 00:50:48 +0700

    cek session di admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,16 @@
-"use client";
-
 import AdminPanel from "@/components/admin/AdminPanel";
+import { getMySession, isSessionExpired } from "@/lib/session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const session = await getMySession();
+  if (session) {
+    if (isSessionExpired(session)) {
+      (await cookies()).set("session", "", { expires: new Date(0) });
+      return redirect("/adminlogin");
+    }
+  }
+  if (!session) return redirect("/adminlogin");
   return <AdminPanel />;
 }
